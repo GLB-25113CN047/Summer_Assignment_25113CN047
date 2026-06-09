e44854bde236246f075ba8c2ea9618ba926aa947
25113cn047-design <25113cn047@glbitm.ac.in>
2026-06-09 22:33:35 +0530

    Assignment-50

---
+++ Assignment-50.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a[100];
+    int n;
+    int s=0;
+    float avg;
+    printf("Enter the Number n:");
+    scanf("%d",&n);
+    printf("Enter the Array a:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        s=s+a[i];
+    }
+    avg=s/n;
+    printf("sum=%d",s);
+    printf("\nAvergae=%f",avg);
+    return 0;
+}
