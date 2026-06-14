6ee0f75c4c74632ef77f6b0dd21489ed7f9116b5
25113cn047-design <25113cn047@glbitm.ac.in>
2026-06-14 17:31:52 +0530

    Assignment-66

---
+++ Day 17/Assignment-66.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int a[100];
+    int b[100];
+    int n;
+    int m;
+    printf("Enter the Size of array n:");
+    scanf("%d",&n);
+    printf("Enter the Array a:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the Array b;");
+    for(int i=0;i<n;i++){
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    for(int i=0;i<n;i++){
+        m=0;
+        for(int j=0;j<n;j++){
+            if(a[j]==b[i]){
+                m=1;
+                break;
+            }
+        }
+        if(m==0){
+            printf("%d ",b[i]);
+        }    
+    }
+    return 0;
+}
