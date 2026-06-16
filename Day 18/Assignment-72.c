239b19839eeffdcf5307b8a36e4c9dc0110a4ca6
25113cn047-design <25113cn047@glbitm.ac.in>
2026-06-16 11:53:52 +0530

    Assignment-72

---
+++ Day 18/Assignment-72.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a[100];
+    int n;
+    int temp;
+    printf("Enter the size of array n:");
+    scanf("%d",&n);
+    printf("Enter the Array a:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(a[j]<a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf(" %d",a[i]);
+    }
+}
