e3a02626190bc984ec2f50c7d30d3c53051008ce
25113cn047-design <25113cn047@glbitm.ac.in>
2026-06-17 23:35:45 +0530

    Assignment-80

---
+++ Day 20/Assignment-80.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a[100][100];
+    int r,c;
+    printf("Enter the row r:");
+    scanf("%d",&r);
+    printf("Enter th the column c:");
+    scanf("%d",&c);
+    printf("Ente the First Matrix a:\n");
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++)
+    {
+        int sum=0;
+        for(int j=0;j<c;j++)
+        {
+            sum=sum+a[j][i];
+        }
+        printf("sum is%d \n",sum);
+    }
+    return 0;
+}
